f4fb62aafdf1034fdcb905276822bb9282b9000d
Victorgr26 <victoralejandrogr26@gmail.com>
2024-09-11 18:27:15 -0400

    DONE

---
+++ typescriptFixme.js
@@ -0,0 +1,55 @@
+// add the type annotation to explicitly specify the type of the variable:
+var isDone = false;
+var lines = 42;
+var username = "Anders";
+// annotate the type as when it's not known
+var unknownTypeVariable;
+//annotate this array
+var list = [1, 2, 3];
+// annotate the return type of this function
+function littleHorribleAlert() {
+    alert("I am a little annoying box!!");
+}
+var f1 = function (i) {
+    return i * i;
+};
+//change f4 to accept a string or a number as a parameter
+function f2(i) {
+    console.log("The value was " + i);
+}
+var person1 = { name: "Bobby", move: function () { } };
+var person2 = { name: "Bobby", age: 42, move: function () { } };
+// Annotate mySearch to describe a function that accepts two string parameters
+var mySearch = function (src, sub) {
+    return src.search(sub) != -1;
+};
+//annotate the constructor arguments and properties
+var Point = /** @class */ (function () {
+    function Point(x, y) {
+        if (y === void 0) { y = 0; }
+        this.x = 0;
+        this.y = 0;
+        this.x = x;
+        this.y = y;
+    }
+    Point.prototype.dist = function () {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    };
+    return Point;
+}());
+// annotate `PointPerson` to use all properties of `Person` and then add the missing required property
+var PointPerson = /** @class */ (function () {
+    function PointPerson(name) {
+        this.name = name;
+    }
+    return PointPerson;
+}());
+// annotate this array
+var arrayOfAnyType = [1, "string", false];
+// fix the foo object so bar and baz can be added
+var foo = {
+    bar: 0,
+    baz: ""
+};
+foo.bar = 123;
+foo.baz = "hello world";
--- typescriptFixme.ts
+++ typescriptFixme.ts
@@ -1,60 +1,73 @@
 // add the type annotation to explicitly specify the type of the variable:
-var isDone = false;
-var lines = 42;
-var username = "Anders";
+var isDone: boolean = false;
+var lines: number = 42;
+var username: string = "Anders";
 
 // annotate the type as when it's not known
-let unknownTypeVariable;
+let unknownTypeVariable: any;
 
 //annotate this array
-var list = [1, 2, 3];
+var list: number[] = [1, 2, 3];
 
 // annotate the return type of this function
-function littleHorribleAlert() {
+function littleHorribleAlert(): void {
   alert("I am a little annoying box!!");
 }
 
-var f1 = function (i) {
+var f1 = function (i: number): number {
   return i * i;
 };
 
 //change f4 to accept a string or a number as a parameter
-function f2(i) {
+function f2(i: string | number): void {
   console.log("The value was " + i);
 }
 
 // Annotate the person objects with an interface. Some properties are optional
-var person1 = { name: "Bobby", move: function () {} };
-var person2 = { name: "Bobby", age: 42, move: function () {} };
+interface Person {
+  name: string;
+  age?: number;
+  move: () => void;
+}
+
+var person1: Person = { name: "Bobby", move: function () { } };
+var person2: Person = { name: "Bobby", age: 42, move: function () { } };
 
 // Annotate mySearch to describe a function that accepts two string parameters
-var mySearch = function (src, sub) {
+var mySearch: (src: string, sub: string) => boolean = function (src, sub) {
   return src.search(sub) != -1;
 };
 
 //annotate the constructor arguments and properties
 class Point {
-  x = 0;
-  constructor(x, y = 0) {
+  x: number = 0;
+  y: number = 0;
+  constructor(x: number, y: number = 0) {
     this.x = x;
     this.y = y;
   }
-  dist() {
+  dist(): number {
     return Math.sqrt(this.x * this.x + this.y * this.y);
   }
 }
 
 // annotate `PointPerson` to use all properties of `Person` and then add the missing required property
-class PointPerson {
-  constructor(name) {
+class PointPerson implements Person {
+  name: string;
+  age?: number;
+  move: () => void;
+  constructor(name: string) {
     this.name = name;
   }
 }
 
 // annotate this array
-var arrayOfAnyType = [1, "string", false];
+var arrayOfAnyType: any[] = [1, "string", false];
 
 // fix the foo object so bar and baz can be added
-let foo = {};
+let foo: { bar: number, baz: string } = {
+  bar: 0,
+  baz: ""
+};
 foo.bar = 123;
 foo.baz = "hello world";

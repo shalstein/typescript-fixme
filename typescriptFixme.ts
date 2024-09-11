72e8f3e089005ddccdf78905c5d1f6462dc100fc
mattiolo <mateus.mattiolo@gmail.com>
2024-09-11 19:02:08 -0400

    update code

--- typescriptFixme.ts
+++ typescriptFixme.ts
@@ -1,60 +1,80 @@
-// add the type annotation to explicitly specify the type of the variable:
-var isDone = false;
-var lines = 42;
-var username = "Anders";
+// Explicit type annotation for boolean, number, and string variables
+var isDone: boolean = false;
+var lines: number = 42;
+var username: string = "Anders";
 
-// annotate the type as when it's not known
-let unknownTypeVariable;
+// Annotate the type when it's not known
+let unknownTypeVariable: any;
 
-//annotate this array
-var list = [1, 2, 3];
+// Annotate this array as an array of numbers
+var list: number[] = [1, 2, 3];
 
-// annotate the return type of this function
-function littleHorribleAlert() {
+// Annotate the return type of this function (void since it doesn't return anything)
+function littleHorribleAlert(): void {
   alert("I am a little annoying box!!");
 }
 
-var f1 = function (i) {
+// Annotate the parameter and return type for f1
+var f1 = function (i: number): number {
   return i * i;
 };
 
-//change f4 to accept a string or a number as a parameter
-function f2(i) {
+// Change f2 to accept a string or a number as a parameter
+function f2(i: number | string): void {
   console.log("The value was " + i);
 }
 
-// Annotate the person objects with an interface. Some properties are optional
-var person1 = { name: "Bobby", move: function () {} };
-var person2 = { name: "Bobby", age: 42, move: function () {} };
+// Annotate the person objects with the Person interface
+interface Person {
+  name: string;
+  age?: number; // Optional property
+  move(): void;
+}
+
+// Explicitly typing person1 and person2 with the Person interface
+var person1: Person = { name: "Bobby", move: function () {} };
+var person2: Person = { name: "Bobby", age: 42, move: function () {} };
 
 // Annotate mySearch to describe a function that accepts two string parameters
-var mySearch = function (src, sub) {
-  return src.search(sub) != -1;
+var mySearch: (src: string, sub: string) => boolean = function (src: string, sub: string): boolean {
+  return src.search(sub) !== -1;
 };
 
-//annotate the constructor arguments and properties
+// Annotate the constructor arguments and properties in Point
 class Point {
-  x = 0;
-  constructor(x, y = 0) {
+  x: number;
+  y: number;
+
+  constructor(x: number = 0, y: number = 0) {
     this.x = x;
     this.y = y;
   }
-  dist() {
+
+  dist(): number {
     return Math.sqrt(this.x * this.x + this.y * this.y);
   }
 }
 
-// annotate `PointPerson` to use all properties of `Person` and then add the missing required property
-class PointPerson {
-  constructor(name) {
+// Annotate PointPerson to use all properties of Person and include the required property 'name'
+class PointPerson implements Person {
+  name: string;
+  age?: number;
+  move(): void {}
+
+  constructor(name: string) {
     this.name = name;
   }
 }
 
-// annotate this array
-var arrayOfAnyType = [1, "string", false];
+// Annotate this array as an array of any type
+var arrayOfAnyType: any[] = [1, "string", false];
 
-// fix the foo object so bar and baz can be added
-let foo = {};
+// Define Foo interface with optional properties bar and baz
+interface Foo {
+  bar?: number;
+  baz?: string;
+}
+
+let foo: Foo = {};
 foo.bar = 123;
 foo.baz = "hello world";
